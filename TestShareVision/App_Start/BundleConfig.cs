44056a63adc2971f5f1003d931584c64f101036d
Emilio Silva <ailtonemilio@hotmail.com>
2021-09-21 13:34:58 -0700

    Insert Toastr JS message

--- TestShareVision/App_Start/BundleConfig.cs
+++ TestShareVision/App_Start/BundleConfig.cs
@@ -19,12 +19,14 @@ namespace TestShareVision
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap")
+                .Include("~/Scripts/bootstrap.js")
+                .Include("~/Scripts/toastr.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css")
+                .Include("~/Content/toastr.css"));
         }
     }
 }
