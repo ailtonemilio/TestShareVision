ec10261ce31f58773b590d77d526e17674987e48
Emilio Silva <ailtonemilio@hotmail.com>
2021-09-21 13:13:48 -0700

    Initial Commit

---
+++ TestShareVision/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestShareVision.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Test(bool returnNum)
+        {
+            int _returnNum = 0;
+
+            if (returnNum)
+                _returnNum = 1;
+
+            return Json(new { success = true, _returnNum},
+                JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
